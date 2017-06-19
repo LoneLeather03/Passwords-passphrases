9a6e98aabe31828ae8cca4ab33fe647839c5c6a4
LoneLeather03 <davidmartinez333@gmail.com>
2017-06-19 09:53:37 -0600

    Array list addition to Wordlist program.

--- src/edu/cnm/deepdive/security/Wordlist.java
+++ src/edu/cnm/deepdive/security/Wordlist.java
@@ -1,12 +1,18 @@
 package edu.cnm.deepdive.security;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class Wordlist {
 
   public static final int RECOMMENDED_PHRASE_LEGNTH = 5;
+
+  public static final String WORD_LIST_FILE = "resources/eff_large_wordlist.txt";
 
   private static final String PROPERTIES_FILE = "resources/text.properties";
 
@@ -23,18 +29,22 @@ public class Wordlist {
       } else if (phraselength < RECOMMENDED_PHRASE_LEGNTH) {
         System.out.println(warningMessage);
       }
+      String[] wordlist = getWordList(WORD_LIST_FILE);
+      System.out.println(Arrays.toString(wordlist)); // FIXME - Get rid of this debugging.
     } catch (NumberFormatException ex) {
       ex.printStackTrace();
       System.out.println(errorMessage);
+      System.out.printf(usageMessage);
       System.exit(1);
     } catch (IllegalArgumentException ex) {
       System.out.println(errorMessage);
-    }  catch (IOException ex) {
-        ex.printStackTrace();
-        System.exit(1);
-      }
+      System.out.printf(usageMessage, Wordlist.class.getName());
+    } catch (IOException ex) {
+      ex.printStackTrace();
+      System.exit(1);
     }
-  
+  }
+
 
   private static void loadResources() throws IOException {
     Properties properties = new Properties();
@@ -46,7 +56,16 @@ public class Wordlist {
     }
   }
 
-
+  public static String[] getWordList(String listPath) throws IOException {
+    try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(Wordlist.class.getClassLoader().getResourceAsStream(listPath)))) {
+      ArrayList<String> words = new ArrayList<>();
+      for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+        words.add(line.split("\\s+")[1]);
+      }
+      return words.toArray(new String[]{});
+    }
+  }
 }
 
 
