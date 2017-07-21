241886a992089c0eaa7c0be9c59bb9bab534695d
LoneLeather03 <davidmartinez333@gmail.com>
2017-07-21 14:24:00 -0600

    Added tab pane and check boxes to UI.

--- src/edu/cnm/deepdive/security/GuardUI.java
+++ src/edu/cnm/deepdive/security/GuardUI.java
@@ -6,9 +6,12 @@ package edu.cnm.deepdive.security;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -20,8 +23,26 @@ public class GuardUI extends Application {
 	private static final String STYLE_SHEET = "/styles/application.css";
 	private static final String LAYOUT_RESOURCE = "/layouts/Generate.fxml";
 	private static final String UI_BUNDLE = "resources/ui";
+	private static final String WINDOW_TITLE_KEY = "window.title";
 
+	@FXML
+	private VBox passwordOptions;
+	@FXML 
+	 private VBox passphraseOption;
+	@FXML
+	 private CheckBox upperIncluded;
+	@FXML
+	 private CheckBox lowerIncluded;
+	@FXML
+	 private CheckBox punctuationIncluded;
+	@FXML
+	 private CheckBox ambiguousExcluded;
+	
+	private SecurePasswordGenerator passwordGenerator = new SecurePasswordGenerator();
+		
+	
 	/* (non-Javadoc)
+	
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
@@ -31,7 +52,12 @@ public class GuardUI extends Application {
 		Scene scene = new Scene(pane, 400, 400);
 		scene.getStylesheets().add(getClass().getResource(STYLE_SHEET).toExternalForm());
 		primaryStage.setScene(scene);
+		primaryStage.setTitle(uiBundle.getString(WINDOW_TITLE_KEY));
 		primaryStage.show();
+//		upperIncluded.setSelected(passwordGenerator.isUpperCaseIncluded());
+//		lowerIncluded.setSelected(passwordGenerator.isUpperCaseIncluded());
+//		punctuationIncluded.setSelected(passwordGenerator.isUpperCaseIncluded());
+//		ambiguousExcluded.setSelected(passwordGenerator.isUpperCaseIncluded());
 	}
 
 	/**
